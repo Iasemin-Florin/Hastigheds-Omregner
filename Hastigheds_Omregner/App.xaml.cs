621434feb73dd2a25b8fbd44777b3665ba2ab3ce
Iasemin-Florin <queue.triple@gmail.com>
2020-11-16 15:22:47 +0100

    Har lavet UI/Design har brugt et gammelt projekt som Inspiration

---
+++ Hastigheds_Omregner/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Hastigheds_Omregner
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
